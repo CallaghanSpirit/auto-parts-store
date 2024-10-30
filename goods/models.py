9f361ac08691e99069e08b162f60e487d09eb552
Alan Nouman <tsoloevalan@gmail.com>
2024-10-30 18:10:41 +0300

    update models in goods

--- goods/models.py
+++ goods/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Goods(models.Model):
-    name = models.TextField()
-    cats = models.IntegerField()
+    name = models.CharField(max_length=255)
+    cats = models.IntegerField(default=0)
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        ordering = ['-time_create']
+        indexes = [
+            models.Index(fields=['-time_create'])
+        ]
