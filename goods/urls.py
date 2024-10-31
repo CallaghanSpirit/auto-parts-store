abfcde7b4a4b22d3cb14cf5c57c308550f231e29
Alan Tsoloev <tsoloevalan@gmail.com>
2024-10-31 22:16:15 +0300

    add slug url

--- goods/urls.py
+++ goods/urls.py
@@ -3,5 +3,6 @@ from goods import views
 
 urlpatterns = [
     path("",views.index, name='home'),
+    path("card-page/<slug:gd_slug>/",views.cardpage,name='card-page'),
     path("category/",views.category, name='cats')
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse,get_object_or_404
+from goods.models import Goods
 
 # Create your views here.
 def index(request):
@@ -9,9 +10,10 @@ def category(request):
     data = {'title':'Категории'}
     return render(request,template_name='goods/category.html',context=data)
 
-def cardpage(request):
-    goods= get_object_or_404()
-    return render(request,template_name='goods/cardpage.html')
+def cardpage(request,gd_slug):
+    goods = get_object_or_404(Goods,slug=gd_slug)
+    data = {"goods":goods}
+    return render(request,template_name='goods/cardpage.html',context=data)
 
 cats = [{'id':1,'name':'Кексы'},
         {'id':2, 'name':'Блины'},
