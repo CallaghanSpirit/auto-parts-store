3e55b01bd47eecd42cf9a505ace140c9fbb3885e
Alan Nouman <tsoloevalan@gmail.com>
2025-01-31 17:14:57 +0300

    Add labels to form

--- goods/forms.py
+++ goods/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Category
 
 class AddPostForm(forms.Form):
-    name = forms.CharField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    status = forms.BooleanField(required=False)
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
+    name = forms.CharField(max_length=255,label='Имя')
+    content = forms.CharField(widget=forms.Textarea(), required=False,label='Описание')
+    status = forms.BooleanField(required=False,initial=True,label='Статус')
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(),empty_label='Без категории',label='Категория')
