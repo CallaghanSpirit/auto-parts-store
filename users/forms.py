5e52d5c20eb64fc7ab5e83d862c9a59e76f43004
Alan Nouman <tsoloevalan@gmail.com>
2025-02-19 17:39:30 +0300

    add new User

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import PasswordChangeForm
+from datetime import date
 
 
 class LoginUserForm(AuthenticationForm):
@@ -32,10 +33,11 @@ class RegisterUserForm(UserCreationForm):
 class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True,label='Логин')
     email = forms.EmailField(disabled=True)
+    this_year = 
 
     class Meta:
         model = get_user_model()
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['photo','username', 'email','date_birth', 'first_name', 'last_name']
         labels = {
                 'first_name':'Имя',
                 'last_name':'Фамилия',
