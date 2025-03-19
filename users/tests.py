00e709a8df07063b172f9cb7b77f6b0a9ab8948a
Alan Nouman <tsoloevalan@gmail.com>
2025-03-19 11:10:43 +0300

    add another test to users pt2

--- users/tests.py
+++ users/tests.py
@@ -8,8 +8,15 @@ from django.contrib.auth import get_user_model
 # Create your tests here.
 class RegisterUserTestCase(TestCase):
     def setUp(self):
-        "Инициализация перед выполнением каждого теста"
-        return super().setUp()
+        self.registration_url = reverse('users:register')  # Замените 'registration' на имя вашего URL для регистрации
+        self.login_url = reverse('users:login')  # Замените 'login' на имя вашего URL для авторизации
+        self.user_model = get_user_model()
+        self.data = {
+            'username': 'testuser',
+            'password1': 'testpassword123',
+            'password2': 'testpassword123',
+        }
+
     # def test_form_register(self):
     #     path = reverse('users:register')
     #     response = self.client.get(path)
@@ -17,22 +24,18 @@ class RegisterUserTestCase(TestCase):
     #     self.assertTemplateUsed(response, 'users/register.html')
 
     def test_user_registration_success(self):
-        data = {
-            'username':'Alan',
-            'email': 'tsoloevalanoid@gmail.com',
-            'first_name': 'Сергей',
-            'last_name':'Гей',
-            'password':'Mikado2006',
-            'password2':'Mikado2006',
+        
+        response = self.client.post(self.registration_url, self.data, follow=True)
+        self.assertRedirects(response, self.login_url)
+        self.assertTrue(self.user_model.objects.filter(username='testuser').exists())
 
-        }
-        user_model = get_user_model()
-
-        path = reverse('users:register')
-        response = self.client.post(path, data, follow=True)
+    def test_registration_test(self):
+        self.data['password2']= 'testpassword12'
+        response = self.client.post(self.registration_url, self.data)
         self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertRedirects(response, reverse('users:login'))
-        self.assertTrue(user_model.objects.get(usename=data['username']))
+        self.assertContains(response, "Введенные пароли не совпадают")
+
+    
 
     def tearDown(self):
         "Действия после выполнения каждого теста"
